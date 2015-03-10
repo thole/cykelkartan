f301006afcfdb76b177730358bb7a87065b46a8c
Thole <ts@tholeschneider.de>
2015-03-10 23:59:56 +0100

    prototype ...

---
+++ data/clean.js
@@ -0,0 +1,18 @@
+var filename = process.argv[2];
+var activities = require('./' + filename);
+var result = new Array()
+
+activities.forEach(function(day){
+	var newDay = JSON.parse(JSON.stringify(day));
+	delete newDay.segments;
+	
+	var newSummary = new Array();
+	day.summary.forEach(function(summary){
+		if(summary.activity !== 'cycling'){return;}
+		newSummary.push(JSON.parse(JSON.stringify(summary)));
+	})
+	newDay.summary = newSummary;
+	result.push(newDay);	
+	
+})
+console.log(JSON.stringify(result,null,2));
